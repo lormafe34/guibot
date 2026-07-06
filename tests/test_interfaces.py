3705c5790894d013beac761c8a45c4657f581578
Lormafe <lormafel@gmail.com>
2026-07-06 19:17:49 +0800

    Decouple autopy import safely for tests

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -14,13 +14,18 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with guibot.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import unittest
 import sys
 import inspect
 from unittest import main, mock, TestCase
 
 import common_test
 
-
+try:
+    import autopy
+except ImportError:
+    autopy = None
 class SimpleAPITest(TestCase):
 
     def setUp(self) -> None:
@@ -64,7 +69,7 @@ class SimpleAPITest(TestCase):
         self.assertEqual(buttons.mod.MOD_SHIFT, "")
         self.assertEqual(buttons.mouse.LEFT_BUTTON, 1)
 
-
+@unittest.skipIf(os.environ.get('DISABLE_AUTOPY', "0") == "1", "AutoPy disabled")
 class ProxyAPITest(TestCase):
 
     def setUp(self) -> None:
